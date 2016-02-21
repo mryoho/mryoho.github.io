947991f9a3a295da2a27e4ca7e0032a37f1f6e48
Patrick Yoho <trickyoho@gmail.com>
2016-02-21 02:40:55 +0000

    More unified theme

---
+++ js/codecademy_name/main.js
@@ -0,0 +1,23 @@
+/*
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+*/
+var orange = [23.9, 99.2, 47.6];
+var darkergreen = [174.1, 93.8, 31.8];
+var red = [0, 100, 63];
+var lightgreen = [122.4, 39.4, 49.2];
+
+var myName = "Mr. Yoho";
+var letterColors = [lightgreen, orange, darkergreen];
+if(15 > 5) {
+    bubbleShape = "circle";
+}
+else {
+    bubbleShape = "square";
+}
+
+drawName(myName, letterColors);
+bounceBubbles();
